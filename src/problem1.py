7a23a6b1c234367a17252d956763a23263de0d99
zouz-sean <zouz@rose-hulman.edu>
2018-05-24 13:20:10 -0400

    final

--- src/problem1.py
+++ src/problem1.py
@@ -2,9 +2,9 @@
 Final exam, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Zhengxiao Zou.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -19,7 +19,7 @@ def main():
     print()
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
-    # run_test_problem1a()
+    run_test_problem1a()
     # run_test_problem1b()
 
 
@@ -93,6 +93,16 @@ def problem1a(numbers):
     # TODO: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    add = 0
+    length = len(numbers)
+    #print(length)
+    for k in range (0,length,(length-1)//2):
+        wanted = numbers[k]
+        #print(wanted)
+        add = add + wanted
+        #print(add)
+    return add
+
 
 
 def run_test_problem1b():
