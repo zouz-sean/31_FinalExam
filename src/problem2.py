cc41cc2b39e28d478ed343541484356558e8ebac
zouz-sean <zouz@rose-hulman.edu>
2018-05-24 13:32:49 -0400

    final

--- src/problem2.py
+++ src/problem2.py
@@ -2,9 +2,9 @@
 Final exam, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Zhengxiao Zou.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -19,7 +19,7 @@ def main():
     print()
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
-    # run_test_problem2a()
+    run_test_problem2a()
     # run_test_problem2b()
     # run_test_problem2c()
 
@@ -170,9 +170,16 @@ def problem2a(x, sequence):
       :type sequence:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    length = len(sequence)
+    answer = []
+    for k in range (length):
+        if sequence[k] > x:
+            answer.append(sequence[k])
+    return answer
+
 
 
 def run_test_problem2b():
